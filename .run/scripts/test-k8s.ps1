7997a46e978042e6d9bb95926f115c0c390355b1
Dominique Boeckli <dboeckli@gmail.com>
2026-09-13 15:49:10 +0200

    chore: Runner + Renovate
    - clear docker/deploy-k8s/uninstall-k8s: ShConfigurationType -> PowerShellRunType
    - tgz-Filter spring-with-micrometer-tracing-chart-*.tgz
    - neuer test-k8s-Runner (helm test)
    - Renovate: ignorePaths + shfmt-Custom-Manager
    
    Refs #62

--- .run/scripts/test-k8s.ps1
+++ .run/scripts/test-k8s.ps1
@@ -2,5 +2,5 @@ cd target/helm/repo
 
 $file = Get-ChildItem -Filter spring-with-micrometer-tracing-chart-*.tgz | Select-Object -First 1
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
-
-helm uninstall $APPLICATION_NAME --namespace spring-with-micrometer-tracing
+Write-Host "test application: $APPLICATION_NAME"
+helm test $APPLICATION_NAME --namespace spring-with-micrometer-tracing --logs
